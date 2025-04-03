5d3872c54f78a103662c736f175ff33d211c0cc1
ARDC USER <ardc@purdue.edu>
2025-04-03 17:06:14 -0400

    checked on ARDC computer, needs integration into end visit

--- Reflexes/RecordReflexes.m
+++ Reflexes/RecordReflexes.m
@@ -9,7 +9,7 @@ orig_path = pwd;
 filename = char(filename);
 
 % Load GUI to enter the reflex thresholds
-global output_reflex
+global output_reflex output_wrs output_act
 getSelectedRadioButtons; 
 uiwait(gcf)
 
@@ -18,6 +18,11 @@ reflex_data.researcher = researcher;
 reflex_data.time = start_time;
 reflex_data = output_reflex; 
 clear output_reflex; 
+
+reflex_data.wrs = output_wrs; 
+reflex_data.act = output_act; 
+clear output_wrs; 
+clear output_act; 
 
 cd(dataDir)
 save([filename,'.mat'],'-struct','reflex_data');
